83b7683d968d86cb17782376877a41cc24c63acd
SDG258 <46186797+SDG258@users.noreply.github.com>
2020-07-07 09:56:20 +0700

    add_upload_avatar

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
+var path = require('path');
 const db = require('./services/db');
 
 const port = process.env.PORT || 3000;
@@ -17,7 +18,7 @@ app.use(cookieSession({
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 //Auth middlewares
 app.use(require('./middlewares/auth'));
 
