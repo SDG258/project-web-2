22b270a6ff2b0f5a457e9199a65c1271c368044d
SDG258 <46186797+SDG258@users.noreply.github.com>
2020-11-18 14:55:22 +0700

    connect db

--- services/db.js
+++ services/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connectString = 'postgres://postgres:postgres@localhost:5432/mybank';
+const connectString = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/mybank';
 const db = new Sequelize(connectString);
 
 module.exports = db;
